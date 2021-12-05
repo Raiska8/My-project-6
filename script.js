db4c050b6595616bc7edd1d43babe4b2541d9370
RAISA ABDRAI KYZY <87840244+Raiska8@users.noreply.github.com>
2021-12-06 02:11:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let isMarried = true;
 
+console.log(isMarried);
+console.log(typeof isMarried);
+
+let cart = null;
+
+console.log(cart);
+console.log(typeof cart);
+
+
+let userName;
+
+console.log(userName);
+console.log(typeof userName);
+
+let myNum = 123;
+console.log(myNum);
+console.log(typeof myNum);
+
+let myStrNum = String(myNum);
+console.log(myStrNum);
+console.log(typeof myStrNum);
